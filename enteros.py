44ab609d6c53b2dd53448ac7a207d2d2d068bebc
JoseMg <joseutp14@gmail.com>
2019-10-11 10:11:39 -0600

    Inicio en Python

---
+++ enteros.py
@@ -0,0 +1,17 @@
+number_one = 10
+number_two = 4
+
+result = number_one + number_two
+print("addition",result)
+
+result = number_one - number_two
+print("subtraction",result)
+
+result = number_one * number_two
+print("multiplication",result)
+
+result = number_one / number_two
+print("division",result)
+
+result = number_one ** number_two
+print("exponenciation",result)
