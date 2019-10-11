26e0135a8c59868daa0d32d5713cf53bdc2719fe
JoseMg <joseutp14@gmail.com>
2019-10-11 08:59:14 -0600

    Variables in Python

---
+++ variables.py
@@ -0,0 +1,16 @@
+#Variables never start with uppercase, example= Course
+#other case is (coursePython = incorrect), (course_python = correct)
+
+
+#python have dynamic typing
+
+course  = "Python 3"#here "course" take the value of "Python 3" type string
+
+course  = 2214#here "course" take the value of "2214" type integer
+
+course  = True#here "course" take the value of "True" type boolean
+             # that is known as dynamic typing
+
+print(course)#print variable
+
+
